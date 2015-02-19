49911cc009c436e358f92d784b0b9a1db8da9e86
Remsnet LTD, <remsnet@users.noreply.github.com>
2015-02-19 16:35:46 +0100

    Update mysql_setup_systemtables.sh

--- mysql_setup_systemtables.sh
+++ mysql_setup_systemtables.sh
@@ -1,2 +1,15 @@
 #
+# Copyright (c) 1995-2015 Remsnet Consulting & Internet Services LTD
+# Author:  Remsnet LTD Support <support at remsnet dot de>
+#
+# All modifications and additions to the file contributed by third parties
+# remain the property of their copyright owners, unless otherwise agreed
+# upon. The license for this file, and modifications and additions to the
+# file, is the same license as for the pristine package itself (unless the
+# license for the pristine package is not an Open Source License, in which
+# case the license is the MIT License). An "Open Source License" is a
+# license that conforms to the Open Source Definition (Version 1.9)
+# published by the Open Source Initiative.
+# Please submit bugfixes or comments via https://github.com/remsnet/mysql_ndb_cluster_helper
+
 sudo su - mysql ;cd /usr/local/mysql; ./scripts/mysql_install_db --basedir=/usr/local/mysql --force --ldata=data
