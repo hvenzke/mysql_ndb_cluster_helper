7a4c3f62874b22e3f89cea416af2affcbb757520
Remsnet LTD, <remsnet@users.noreply.github.com>
2015-02-19 16:34:19 +0100

    Update start_fix_rootperm__mysqld.sh

--- start_fix_rootperm__mysqld.sh
+++ start_fix_rootperm__mysqld.sh
@@ -2,6 +2,19 @@
 
 # http://dev.mysql.com/doc/refman/5.1/de/resetting-permissions.html
 
+# Copyright (c) 1995-2015 Remsnet Consulting & Internet Services LTD
+# Author:  Remsnet LTD Support <support at remsnet dot de>
+#
+# All modifications and additions to the file contributed by third parties
+# remain the property of their copyright owners, unless otherwise agreed
+# upon. The license for this file, and modifications and additions to the
+# file, is the same license as for the pristine package itself (unless the
+# license for the pristine package is not an Open Source License, in which
+# case the license is the MIT License). An "Open Source License" is a
+# license that conforms to the Open Source Definition (Version 1.9)
+# published by the Open Source Initiative.
+# Please submit bugfixes or comments via https://github.com/remsnet/mysql_ndb_cluster_helper
+
 PATH=$PATH:/usr/local/mysql/bin
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/mysql/lib
 export PATH LD_LIBRARY_PATH
